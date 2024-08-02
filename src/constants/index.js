476a44a3a5bdcacb11977bd4b88729aded9bc4c4
zeeshan <muhammad.zeeshan@brainxtech.com>
2024-08-02 19:05:33 +0500

    api url change

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,5 +3,4 @@ export const modelObjects = {
   backImage: 'backImage',
 };
 
-export const API_BASE_URL =
-  'https://dalle-54zs9m790-zee-db4177e9.vercel.app/api/v1';
+export const API_BASE_URL = 'https://dalle-henna.vercel.app/api/v1';
